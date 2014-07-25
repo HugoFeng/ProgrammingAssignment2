9f44540a5dfa8f8c41cfe3a47ae293975253c584
Hugo.fxy <hugo.fxy@gmail.com>
2014-07-26 00:54:03 +0200

    Built two functions, the assignment is working now.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below add cache features above the 
+## original "solve" function, which is to calculate
+## the inversion of an inversible square matrix.
 
-## Write a short comment describing this function
+## The first function builds special "vector", which 
+## is represented as a list of functions.
+## (The input matrix is assumed to be inversible.)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    rev <- NULL
+    set <- function(y) {
+        x <<- y
+        rev <<- NULL
+    }
+    get <- function() x
+    setrev <- function(reversed) rev <<- reversed
+    getrev <- function() rev
+    list(set = set, get = get,
+         setrev = setrev,
+         getrev = getrev)
 }
 
 
-## Write a short comment describing this function
+## The second function returns the cached inversed matrix,
+## if there's non, it will first calculate and update the 
+## cached value with the new result, then return it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    rev <- x$getrev()
+    if(!is.null(rev)) {
+        message("getting cached data")
+        return(rev)
+    }
+    data <- x$get()
+    rev <- solve(data, ...)
+    x$setrev(rev)
+    rev
 }
